1395ab5956b3483e19c78620f61ac8183569acd5
samet-byte <sametbayat@hacettepe.edu.tr>
2024-02-01 16:47:07 +0300

    Navigation Added

--- app/src/main/java/com/sametb/hoopsinsight/MainActivity.kt
+++ app/src/main/java/com/sametb/hoopsinsight/MainActivity.kt
@@ -3,13 +3,6 @@ package com.sametb.hoopsinsight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.sametb.hoopsinsight.nav.SetupNavGraph
@@ -26,6 +19,7 @@ class MainActivity : ComponentActivity() {
 
                 navController = rememberNavController()
                 SetupNavGraph(navController = navController)
+
 
             }
         }
